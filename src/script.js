a7ea5d381ccdd4c754b93f015c26c2ca1f22dd17
cybardev <50134239+cybardev@users.noreply.github.com>
2026-06-08 02:23:54 -0300

    feat: note duration
    + improve responsiveness

--- src/script.js
+++ src/script.js
@@ -1,14 +1,23 @@
 function STATE() {
-  const SYNTH = new Tone.Synth().toDestination();
+  let synth = null;
 
   return {
-    baseOctave: 4,
+    baseOctave: 3,
+    noteDuration: 8,
+    durationRange: [1, 2, 4, 8, 16, 32],
+    octaveRange: [2, 3, 4, 5, 6],
     notes: ["D#", "F#", "G#", "A#", "C#"],
+    durationLabel(d) {
+      return `1/${parseInt(d)}`;
+    },
     async playNote(note) {
       await Tone.start();
-      let octave = parseInt(this.baseOctave);
-      let toPlay = note === "C#" ? `${note}${octave + 1}` : `${note}${octave}`;
-      SYNTH.triggerAttackRelease(toPlay, "8n");
+      if (!synth) synth = new Tone.Synth().toDestination();
+      let toPlay =
+        note === "C#"
+          ? `${note}${this.baseOctave + 1}`
+          : `${note}${this.baseOctave}`;
+      synth.triggerAttackRelease(toPlay, `${this.noteDuration}n`);
     },
   };
 }
