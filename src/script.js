98718c00bd1b1083581f1b4157d9c8a15c264b67
cybardev <50134239+cybardev@users.noreply.github.com>
2026-06-08 13:59:35 -0300

    feat: allow playing multiple keys simultaneously (like chords)

--- src/script.js
+++ src/script.js
@@ -5,19 +5,25 @@ function STATE() {
     baseOctave: 3,
     octaveRange: [2, 3, 4, 5, 6],
     notes: ["D#", "F#", "G#", "A#", "C#"],
+    pressed: [],
+    playableNote(note) {
+      return note === "C#"
+        ? `${note}${this.baseOctave + 1}`
+        : `${note}${this.baseOctave}`;
+    },
     async playNote(event) {
       await Tone.start();
-      if (!synth) synth = new Tone.Synth().toDestination();
+      if (!synth) synth = new Tone.PolySynth(Tone.Synth).toDestination();
       const note = event.target.innerText;
-      let toPlay =
-        note === "C#"
-          ? `${note}${this.baseOctave + 1}`
-          : `${note}${this.baseOctave}`;
-      synth.triggerAttack(toPlay);
+      if (!this.pressed.includes(note)) this.pressed.push(note);
+      synth.triggerAttack(this.playableNote(note));
     },
     stopNote(event) {
       event.target.blur();
-      if (synth) synth.triggerRelease();
+      const note = event.target.innerText;
+      this.pressed = this.pressed.filter((n) => n !== note);
+      if (synth)
+        synth.triggerRelease(this.playableNote(event.target.innerText));
     },
   };
 }
